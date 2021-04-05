47f4dac10fcd37206f46b9e396b33ebd019127d2
mahadevharish310 <30906369+mahadevharish310@users.noreply.github.com>
2021-04-05 11:42:29 -0400

    Add files via upload
    Updated script -second-commit-

--- src/script.js
+++ src/script.js
@@ -29,3 +29,46 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Scroll button
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  // target object --btnScrollTo
+  // to get the position/coordinates of the btnScrollTo
+  console.log(e.target.getBoundingClientRect());
+
+  // To get the scrolling of the web page from the viewport.
+  // each scroll will be shown in pixels and from top-tip of the viewport to bottom-tip of the viewport
+  // taking all these offset values
+
+  // cuurent scroll position ---the viewport scroll position
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // setting the value to scroll
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // to make it as nice transition, which is a behaviour
+  // this is kind of oldSchool way of doing the scroll
+  // because we have taken the values of XYOffset and matching it
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // new way of doing the Scrolling
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
